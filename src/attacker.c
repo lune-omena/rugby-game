4a6467ef15194a13da4d381d6edeb452780ca32f
Lucy Anne de Omena Evangelista <lucy.omena@live.com>
2022-03-27 20:02:34 -0300

    Add basic strategies for attacker and defender. Do not consider spy.

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -11,19 +13,77 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define LOCK_STRATEGY 3 // number of round to get stuck and to leave stuck
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+// Strategy is randomly walk to the last column
+direction_t atk_directions[] =  {
+  DIR_STAY,
+  DIR_UP,
+  DIR_UP_RIGHT,
+  DIR_RIGHT,
+  DIR_DOWN,
+  DIR_DOWN_RIGHT
+};
+
+// In case of being stuck, use the stuck directions
+direction_t atk_stuck_directions[] = {
+	DIR_DOWN_LEFT,
+	DIR_LEFT,
+	DIR_UP_LEFT
+};
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
+  srand(time(NULL));
+  static position_t previous_position = INVALID_POSITION;
+  static int previous_dir = 0;
+  static int lucky_number = 0;
+  static int stuck_index = 0; // count rounds of being stucked
+  static int stuck_lock = 0; // lock at stuck strategy for LOCK_STRATEGY rounds
+
+  int dir;
+  direction_t new_position;
+  
+  if (equal_positions(attacker_position, previous_position) && previous_dir != 0) {
+    stuck_index++;
+  }
+
+  if (stuck_index == LOCK_STRATEGY || stuck_lock != 0){
+	// INITIATE STUCK STRATEGY!
+	// start lock status, and do lock strategy moves (retreat for LOCK_STRATEGY turns)
+	if (!stuck_lock) stuck_lock = LOCK_STRATEGY;
+
+	dir = lucky_number % 3;
+    lucky_number = (lucky_number % 3) + rand();
+
+	new_position = atk_stuck_directions[dir];
+
+	// reset lock status
+	stuck_lock--;
+	if (!stuck_lock) stuck_index = 0;
+  } 
+  else {
+    dir = lucky_number % 6;
+    lucky_number = (lucky_number % 10) + rand();
+
+	new_position = atk_directions[dir];
+  }
+
+  // strategy doesn't consider spy
   UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  previous_position = attacker_position;
+  previous_dir = dir;
+
+  return new_position;
 }
+
+
+
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +18,33 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+// Strategy is to intercept the attacker in the last column
+direction_t def_directions[] =  {
+  DIR_STAY,
+  DIR_UP,
+  DIR_DOWN
+};
+
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
+  srand(time(NULL));
+  static int lucky_number = 0;
+
+  direction_t new_position;
+  int dir;
+
+  dir = lucky_number % 3;
+  lucky_number = (lucky_number % 10) + rand();
+
+  new_position = def_directions[dir];
+  
+  // strategy doesn't consider spy use and being stuck
   UNUSED(defender_position);
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  return new_position;
 }
 
 /*----------------------------------------------------------------------------*/
